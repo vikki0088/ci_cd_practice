e1b7c127053af3d5f6d229006c1a36997b27bc95
Tatikonda Vishnu Vardhan <tvardhan@opentext.com>
2026-08-21 01:43:29 +0530

    first commit

---
+++ src/math_operations.py
@@ -0,0 +1,6 @@
+def add(a: int, b: int) -> int:
+    """Add two numbers together."""
+    return a + b
+def subtract(a: int, b: int) -> int:
+    """Subtract the second number from the first."""
+    return a - b
