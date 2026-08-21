0022df16ce2b40ec6beb7597d5ac90454ccf79d0
Tatikonda Vishnu Vardhan <tvardhan@opentext.com>
2026-08-21 12:37:07 +0530

    formated the code

--- src/rest.py
+++ src/rest.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+
 app = FastAPI()
+
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, CI/CD World!"}
--- test/test_fast_api.py
+++ test/test_fast_api.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+
 from src.rest import app
 
 client = TestClient(app)
