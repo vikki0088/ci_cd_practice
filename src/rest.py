8ae3840dbb72ad6a037587d031a486eec919fcd6
Tatikonda Vishnu Vardhan <tvardhan@opentext.com>
2026-08-21 12:33:56 +0530

    added fastapi code

---
+++ src/rest.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"message": "Hello, CI/CD World!"}
+@app.get("/add/{a}/{b}")
+def add_numbers(a: int, b: int):
+    return {"result": a + b}
---
+++ test/test_fast_api.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from src.rest import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, CI/CD World!"}
+
+def test_add_numbers():
+    response = client.get("/add/5/3")
+    assert response.status_code == 200
+    assert response.json() == {"result": 8}
